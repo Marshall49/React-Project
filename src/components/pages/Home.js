9f0858186b2aaa364236bb30160b4a6002251e09
Malcolm Strong <strong.malcolm@yahoo.com>
2018-08-01 23:38:21 -0400

    finished the basis of the Okta auth

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withAuth } from '@okta/okta-react';
 
-export default () => {
-    return (
+export default withAuth(class Home extends Component {
+state = { authenticated: null };
+
+  checkAuthentication = async() => {
+    const authenticated = await this.props.auth.isAuthenticated();
+    if (authenticated !== this.state.authenticated) {
+      this.setState({ authenticated });
+    }
+  }
+
+  async componentDidMount() {
+    this.checkAuthentication();
+  }
+
+  async componentDidUpdate() {
+    this.checkAuthentication();
+  }
+
+  login = async() => {
+    this.props.auth.login('/');
+  }
+
+  logout = async() => {
+    this.props.auth.logout('/');
+  }
+
+  render() {
+    if (this.state.authenticated === null) return null;
+
+    const mainContent = this.state.authenticated ? (
       <div>
-        <h1>Home</h1>
+        <p className="lead">You Have Entered the Staff Portal, <Link to="/staff">Click
+        Here</Link></p>
+        <button className="btn btn-light btn-lg" onClick={this.logout}>Logout</button>
       </div>
-    )
-}
+    ) : (
+      <div>
+        <p className="lead">If you are a Staff Memeber, please get your credentials from
+        your supervisor</p>
+        <button className="btn btn-dark btn-lg" onClick={this.login}>Login</button>
+      </div>
+    );
+
+    return (
+      <div className="jumbotron">
+        <h1 className="display-4">Stuff Going Here</h1>
+        {mainContent}
+      </div>
+    );
+  }
+});
